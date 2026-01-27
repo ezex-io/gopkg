a9323d4cf818dcb3b860f9c0e78a5f87f93def81
Mostafa <mostafa.sedaghat@gmail.com>
2026-01-27 21:49:59 +0800

    feat(signal): add signal package

--- cache/go.mod
+++ cache/go.mod
@@ -2,6 +2,6 @@ module github.com/ezex-io/gopkg/cache
 
 go 1.25.1
 
-require github.com/ezex-io/gopkg/scheduler v0.0.0-20260114174958-d74e6b9e615f
+require github.com/ezex-io/gopkg/scheduler v0.0.0-20260120175238-90dc637d8ae0
 
 require golang.org/x/sync v0.19.0 // indirect
--- util/util.go
+++ util/util.go
@@ -4,32 +4,7 @@ import (
 	"crypto/rand"
 	"errors"
 	"math/big"
-	"os"
-	"os/signal"
-	"syscall"
 )
-
-// TrapSignal traps SIGINT and SIGTERM and terminates the server correctly.
-func TrapSignal(cleanupFunc func()) {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		sig := <-sigs
-		if cleanupFunc != nil {
-			cleanupFunc()
-		}
-		exitCode := 128
-		switch sig {
-		case syscall.SIGINT:
-			exitCode += int(syscall.SIGINT)
-		case syscall.SIGTERM:
-			exitCode += int(syscall.SIGTERM)
-		}
-
-		//nolint:revive // exit on fatal log
-		os.Exit(exitCode)
-	}()
-}
 
 // Character sets.
 const (
